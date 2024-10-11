5dc59b8b3c0dedc37e5e1ebc57118cd90917190d
wh-soares <159548446+wh-soares@users.noreply.github.com>
2024-10-11 08:46:55 -0300

    Add files via upload

---
+++ Flash-main/perguntas.js
@@ -0,0 +1,155 @@
+criaCartao(
+    'CIÊNCIAS',
+    'Qual é a formúla química da água?',
+    'H²O?'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'No que se refere a Primeira Lei de Newton?',
+    'Todo corpo em repouso ou em movimento tende a ficar em seu estado de movimento á não ser que sofra uma força externa'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Qual o livro que Charles Darwin escreveu e mudou a forma como vemos a evolução ?',
+    'A Origem das Especíes'
+)
+criaCartao(
+    'CIÊCIAS',
+    'Qual reino pertence a raposa?',
+    'Reino Animallia'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Do que são formadas as nebulosas?',
+    'Hidrogênio, poeira cósmisca ,plasma.'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Como é denomidada uma molécula orgânica que só apresenta Hidrogênio e Carbono?',
+    'Hidrocarboneto'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á qual das duas pertence o Homem-Areia?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o Homem-Aranha?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel',
+    'Quem é o melhor vilão entre as duas?',
+    'Coringa (DC)'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o vilão denomidado Duas Caras?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o anti-herói chamado de "Pacificador"?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'A frase "Ai eu eu descansaria num Universo Agradecido" foi dita por?',
+    'Vilão da Marvel, Thanos'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual é a fraqueza do Batman?',
+    'As pessoas que ele cria um laço'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual das franquias possui os melhores Super-Heróis?',
+    'DC, não se engane.'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual dos universos tem os melhores filmes?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual dos universos tem as melhores séries?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual a melhor pessoa pro Batman?',
+    'É a Batgirl.'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual o filme que mais acumulou dinheiro pra Marvel',
+    'Vingadores, Ultimato'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é o personagem principal de GTA San Andreas?',
+    'O famoso Carl Johnson (CJ)'
+)
+criaCartao(
+    'JOGOS',
+    'Qual jogo do Mario mais conhecido?',
+    'Super Mario Bros'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é o jogo que roda em qualquer tela?',
+    'Doom'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é o Vilão do Sonic?',
+    'Eggman'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é o Vilão do Mário?',
+    'Browser'
+)
+criaCartao(
+    'JOGOS',
+    'Dos ultimos 2 jogos (Mario e Sonic), qual é melhor?',
+    'Nenhum, ambos são incrivelmente bons.'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é a cor da Marca de Guerra do Kratos?',
+    'Vermelho'
+)
+criaCartao(
+    'JOGOS',
+    'Qual é o Need For Speed mais icônico?',
+    'NFS Underground de PS2'
+)
+criaCartao(
+    'JOGOS',
+    'Quantos jogos do FIFA exitem atualmente?',
+    '19'
+)
+criaCartao(
+    'JOGOS',
+    'E quantos jogos do Sonic?',
+    'Aproximadamente 175'
+)
+criaCartao(
+    'HISTÓRIA',
+    'Quanto tempo durou a Ditadura Militar no Brasil?',
+    'Em torno de 21 anos'
+)
+criaCartao(
+    'HISTÓRIA',
+    'Quando foi a Revolução Russa?',
+    '1917 - 1923'
+)
+criaCartao(
+    'HISTÓRIA',
+    'Quanto tempo durou a Guerra Dos 100 Anos?',
+    '116 anos'
+)
